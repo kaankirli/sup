3bfd4387dbb4f4e9e3f0614b98a8d5c37b2bcfdb
gunes <gunes.buyukgonenc@ozu.edu.tr>
2018-12-19 11:11:57 +0300

    Renamed CUPTest to SupTest
    Separated Test and SupTest Classes (don't make multiple classes in a single file please)
    
    Commented entire SupClass because it's giving compile errors all over for me (uncomment the problem is about me or solve it)

---
+++ src/test/SupTest.java
@@ -0,0 +1,33 @@
+package test;
+
+/*
+
+import controller.ListenerPanelDrawing;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+*/
+public class SupTest {
+
+/*
+	@Test
+	public void testWindowSize() {
+		FrameMain frameMain = new FrameMain();
+		assertEquals(500, frameMain.getHeight());
+		assertEquals(1000, frameMain.getWidth());
+
+	}
+	@Test
+	public void testBrushSizeInit() {
+		FrameMain frameMain = new FrameMain();
+		assertEquals(20, frameMain.getBrushSize());
+
+	}
+	@Test
+	public void testBrushSizeSetter() {
+		FrameMain frameMain = new FrameMain();
+		frameMain.setBrushSize(10);
+		assertEquals(10, frameMain.getBrushSize());
+	}
+*/
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,30 +1,5 @@
 package test;
 
-import controller.ListenerPanelDrawing;
-import org.junit.Test;
-import static org.junit.Assert.*;
+public class Test {
 
-
-public class CUPTest {
-
-    @Test
-    public void testWindowSize() {
-        FrameMain frameMain = new FrameMain();
-        assertEquals(500, frameMain.getHeight());
-        assertEquals(1000, frameMain.getWidth());
-
-    }
-    @Test
-    public void testBrushSizeInit() {
-        FrameMain frameMain = new FrameMain();
-        assertEquals(20, frameMain.getBrushSize());
-
-    }
-    @Test
-    public void testBrushSizeSetter() {
-        FrameMain frameMain = new FrameMain();
-        frameMain.setBrushSize(10);
-        assertEquals(10, frameMain.getBrushSize());
-
-    }
 }
