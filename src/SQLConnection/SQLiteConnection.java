e15b18d6e97cb9b61e0ad809b8ba123b1405cf83
ArmanHZ <arman97@gmail.com>
2018-12-20 03:23:23 +0300

    Added the skeleton of SQLiteConnection class.
    Also added connection related test case.

---
+++ src/SQLConnection/SQLiteConnection.java
@@ -0,0 +1,30 @@
+package SQLConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteConnection {
+	
+	private static Connection CONNECTION = null;
+	private static final String URL = "jdbc:sqlite:Brushes.db";
+	
+	private SQLiteConnection() {
+		
+	}
+	
+	public static Connection getInstance() {
+		if (CONNECTION == null) {
+			try {
+				CONNECTION = DriverManager.getConnection(URL);
+				System.out.println("Connected");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return CONNECTION;
+		}
+		else
+			return CONNECTION;
+	}
+
+}
